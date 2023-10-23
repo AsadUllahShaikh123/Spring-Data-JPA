c0a63017f8fbe37da2d21c5aa17f21e644562f32
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-10-24 02:48:25 +0500

    comment down StudentDaoImpl bcz Spring will create it automatically

--- src/main/java/com/spring_data_jpa/dao/StudentDaoImpl.java
+++ src/main/java/com/spring_data_jpa/dao/StudentDaoImpl.java
@@ -1,24 +1,25 @@
 package com.spring_data_jpa.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring_data_jpa.entities.Student;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
-@Repository
-public class StudentDaoImpl implements StudentDAO{
-
-	@PersistenceContext
-	EntityManager entityManager;
-	
-	@Transactional
-	public void saveStudent(Student student) {
-		System.out.println("Record Inserted ....");
-		entityManager.persist(student);
-		
-	}
-
-}
+//@Repository
+//public class StudentDaoImpl {
+//
+//	@PersistenceContext
+//	@Autowired
+//	EntityManager entityManager;
+//	
+//	@Transactional
+//	public void saveStudent(Student student) {
+//		System.out.println("Record Inserted ....");
+//		entityManager.persist(student);
+//		
+//	}
+//
+//}
